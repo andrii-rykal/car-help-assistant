934ea3ccc6cd29c586954f076f1194a64210eea9
Andrii Rykal <andreuka79@gmail.com>
2025-02-08 19:48:35 +0200

    test deploy

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Header = () => {
-  return <div>It is the Header</div>;
+  return (
+    <div className='text-center'>
+      <h1>It is the Header</h1>
+      <p>Test Deploy</p>
+    </div>
+  )
 };
 
 export default Header;
